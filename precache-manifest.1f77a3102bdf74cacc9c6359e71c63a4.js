7db7f7378f29c153ec706faaf3db4a6451205634
Dalibor <daliborpetric99@gmail.com>
2019-04-07 19:06:36 +0200

    Updates

---
+++ precache-manifest.1f77a3102bdf74cacc9c6359e71c63a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0db4dffe7314528fd5e4",
+    "url": "/quantox/static/js/runtime~main.37b86b09.js"
+  },
+  {
+    "revision": "80ec44c329c792d689e6",
+    "url": "/quantox/static/js/main.69ad47b7.chunk.js"
+  },
+  {
+    "revision": "c3737c0225d0f6abef79",
+    "url": "/quantox/static/js/2.fc8ad4c5.chunk.js"
+  },
+  {
+    "revision": "80ec44c329c792d689e6",
+    "url": "/quantox/static/css/main.a54bf3fc.chunk.css"
+  },
+  {
+    "revision": "41582813fd3954868339ccc777a9f048",
+    "url": "/quantox/index.html"
+  }
+];
